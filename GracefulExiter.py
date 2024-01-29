82f93f4397a2b6677f1f58a652e7fdb10112b48d
Marcel <ligimixi@gmail.com>
2024-01-29 15:22:39 +0100

    added graceful exiter class and serialJsonTest

---
+++ GracefulExiter.py
@@ -0,0 +1,14 @@
+import signal
+
+class GracefulExiter():
+    def __init__(self):
+        self.state = False
+        signal.signal(signal.SIGINT, self.change_state)
+
+    def change_state(self, signum, frame):
+        print("Exit Flag set to True (repeat to exit now)")
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        self.state = True
+
+    def exit(self):
+        return self.state
---
+++ serialTest.py
@@ -0,0 +1,44 @@
+import serial
+import json
+import time
+import sys
+from GracefulExiter import GracefulExiter
+
+# get user input string "ON" or "OFF"
+# input_str = input("Enter ON or OFF: ")
+
+n = 0
+ser = serial.Serial('COM3', 115200, timeout=1)
+time.sleep(2)
+
+def send_command(command):
+    data = {"command": command}
+    ser.write(json.dumps(data).encode('utf-8'))
+    time.sleep(1)
+
+flag = GracefulExiter()
+
+starttime = time.time()
+
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode().strip()
+        try:
+            data = json.loads(line)
+            print(data)
+        except json.decoder.JSONDecodeError:
+            print("JSONDecodeError")
+            print(f"Couldn't decode line: {line}")
+
+    if (time.time() - starttime) > 5:
+        if n % 2 == 0:
+            send_command("ON")
+        else:
+            send_command("OFF")
+        n += 1
+        starttime = time.time()
+
+    if flag.exit():
+        ser.close()
+        sys.exit()
+
